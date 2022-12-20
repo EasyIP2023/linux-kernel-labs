21886127abc4e17366488a056be38e30721bec33
Vincent Davis Jr <vince@underview.tech>
2022-12-20 02:59:46 -0600

    env.sh: remove non-useful lines
    Buzybox does not reconize external CROSS_PREFIX variable.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- env.sh
+++ env.sh
@@ -5,7 +5,3 @@ export PATH="${CDIR}/gcc-arm/bin:$PATH"
 export ARCH="arm"
 export CROSS_COMPILE="arm-linux-gnueabihf-"
 
-# For Busybox
-export CONFIG_PREFIX="$(pwd)/modules/nfsroot"
-
-cd $(pwd)/linux
